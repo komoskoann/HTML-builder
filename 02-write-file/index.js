61ca43b459fac3533d740f2aa77e5da5452261d7
komoskoann\\----------- <komoskoann@gmail.com>
2021-11-03 13:53:40 +0300

    feat: solve 02 task

---
+++ 02-write-file/index.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+const path = require('path').join(__dirname, 'text.txt');
+const stdout = process.stdout;
+const readlinePromises = require('readline');
+const rl = readlinePromises.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+fs.writeFile(
+  path,
+  '',
+  (err) => {
+    if (err) throw err;
+  }
+);
+
+async function writeToFile(path, answer) {
+  if(answer !== 'exit') {
+    fs.appendFile(
+      path,
+      `${answer}`,
+      (err) => {
+        if (err) throw err;
+      }
+    )    
+  } else {
+    sayGoodBuy();
+  }
+}
+
+rl.question(`Hi! Enter some text, please\n`, (answer) => {
+  writeToFile(path, answer);
+})
+
+rl.on('line', (answer) => {
+  writeToFile(path, answer);
+})
+
+async function sayGoodBuy() {
+  stdout.write('Good bye');
+  rl.close();
+}
+
+rl.on('SIGINT', () => {
+  sayGoodBuy();
+})
