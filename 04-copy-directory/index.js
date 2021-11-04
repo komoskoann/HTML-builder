0433a5507a5a00f245853c1a700546047df8a854
komoskoann\\----------- <komoskoann@gmail.com>
2021-11-04 15:08:16 +0300

    feat: solve 04 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,35 @@
+const fs = require('fs').promises;
+const path = require('path');
+const srcDirPath = path.join(__dirname, 'files');
+const destDirPath = path.join(__dirname, 'files-copy');
 
+async function main() {
+  const delDir = await deleteNewDir(destDirPath);
+  const newDir = await createNewDir(destDirPath);
+  const files = await readDir(srcDirPath);
+  files.forEach(processFile);
+}
+
+async function deleteNewDir(path) {
+  return fs.rmdir(path, {recursive: true});
+}
+
+async function createNewDir(path) {
+  return fs.mkdir(path, {recursive: true});
+}
+
+async function readDir(path) {
+  return fs.readdir(path);
+}
+
+async function copyFile(src, dest) {
+  return fs.copyFile(src, dest)
+}
+
+async function processFile(file) {
+  let srcFilePath = path.join(srcDirPath, file);
+  let destFilePath = path.join(destDirPath, file);
+  await copyFile(srcFilePath, destFilePath);
+}
+
+main();
